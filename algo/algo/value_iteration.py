201922cf7d5130d0152a39509bdc3ce017358770
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-05-27 18:27:17 -0400

    More tests and viz functions.

--- algo/algo/value_iteration.py
+++ algo/algo/value_iteration.py
@@ -27,6 +27,8 @@ class ValueIteration:
         self.viz_map[ self.start[0], self.start[1] ] = 's'
         self.viz_map[ self.goal[0], self.goal[1] ] = 'g'
 
+        self.marked_states = []
+
     def reward( self, s_ ):
         assert( s_.dtype == int )
         if np.all( s_ == self.goal ):
@@ -43,6 +45,7 @@ class ValueIteration:
 
     def visualizeMap( self ):
         print( self.viz_map )
+        print("\n")
 
     """The viz_maps before and after the function call should be identical."""
     def visualizeState( self, s_ ):
@@ -50,24 +53,55 @@ class ValueIteration:
         cache = self.viz_map[ s_[0], s_[1] ]
         self.viz_map[ s_[0], s_[1] ] = 'o'
         print( self.viz_map )
+        print("\n")
         self.viz_map[ s_[0], s_[1] ] = cache
 
+    def markState( self, s_ ):
+        self.marked_states.append( s_ )
+        assert( s_[0] < self.n_rows and s_[1] < self.n_cols )
+        self.viz_map[ s_[0], s_[1] ] = 'o'
+        print( self.viz_map )
+        print("\n")
 
-def main():
+    def clearMarkedStates( self ):
+        for state in self.marked_states:
+            self.viz_map[ state[0], state[1] ] = '.'
+
+def runTests():
     val_iter = ValueIteration( np.array( [0, 0], dtype=int ), np.array( [ 5,
             9], dtype=int ) )
     val_iter.visualizeMap()
 
     # Test reward function
+    print( "Testing reward function." )
     state = np.array( (5,9), dtype=int )
     reward = val_iter.reward( state )
     print(reward)
 
+    # Test viz.
+    print( "Testing visualizeState." )
     state = np.array( (2,7), dtype=int )
     val_iter.visualizeState( state )
 
     # Test nextState
-    #nextState = val_iter.nextState( state, )
+    print( "Testing nextState function." )
+    nextState = val_iter.nextState( state, 0 )
+    val_iter.markState( nextState )
+    nextState = val_iter.nextState( state, 1 )
+    val_iter.markState( nextState )
+    nextState = val_iter.nextState( state, 2 )
+    val_iter.markState( nextState )
+    nextState = val_iter.nextState( state, 3 )
+    val_iter.markState( nextState )
+    val_iter.visualizeMap()
+
+def main():
+    runTests()
+    val_iter = ValueIteration( np.array( [0, 0], dtype=int ), np.array( [ 5,
+            9], dtype=int ) )
+    #val_iter.visualizeMap()
+
+
 
 if __name__ == "__main__":
     main()
