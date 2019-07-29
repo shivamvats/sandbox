9d59fe4b3338eebfc872cfccef204d2a59d0766a
Shivam Vats <shivamvats.iitkgp@gmail.com>
2019-07-29 00:53:22 -0400

    Fix goal constraint plot.

--- algo/algo/goal_constraints/nn.py
+++ algo/algo/goal_constraints/nn.py
@@ -24,14 +24,19 @@ def sampleRandomPointsFromRect(n, x_lims, y_lims):
             y_lims[0], y_lims[1] )] for i in range(n) ]
     return points
 
+def plotPoints( points, clr ):
+    points = np.array(points)
+    plt.scatter( points[:, 0], points[:, 1], marker='o', color=clr )
+    plt.show()
+
 def plotDecisionBoundary( points, preds ):
     points = np.array(points)
     points_0 = np.array([ point for point, label in zip(points, preds) if label
                         == 0 ])
     points_1 = np.array([ point for point, label in zip(points, preds) if label
                         == 1 ])
-    plt.scatter( points_0[:, 0], points_0[:, 1], marker='o', color='b' )
-    plt.scatter( points_1[:, 0], points_1[:, 1], marker='o', color='r' )
+    plt.scatter( points_0[:, 0], points_0[:, 1], marker='o', color='r' )
+    plt.scatter( points_1[:, 0], points_1[:, 1], marker='o', color='b' )
     plt.show()
 
 
@@ -40,11 +45,32 @@ if __name__ == "__main__":
     x_lims = [0, 10]
     y_lims = [0, 10]
 
-    X = [[4,4], [5,5], [6,6], [4,7], [7,4], [0,0],
-            [10,10],[10,0],[0,10],[2,9],[2,2],[9,9]]
-    y = [1, 1, 1, 1, 1, 0, 0, 0,0,0,0,0]
+    x_lims_positive = [3, 8]
+    y_lims_positive = [3, 8]
+
+    #X = [[4,4], [5,5], [6,6], [4,7], [7,4], [0,0],
+    #[10,10],[10,0],[0,10],[2,9],[2,2],[9,9]]
+
+    n_positive = 100
+    X_positive = np.array(sampleRandomPointsFromRect( n_positive, x_lims_positive,
+            y_lims_positive ))
+    y_positive = np.ones(n_positive)
+
+
+    n_negative = 100
+    X_negative = np.array(sampleRandomPointsFromRect( 10*n_negative, x_lims,
+            y_lims ))
+    reject_positive = lambda x: ((x[0] < x_lims_positive[0] or x[0] >
+            x_lims_positive[1]) and (x[1] < y_lims_positive[0] or x[1] >
+            y_lims_positive[1]))
+    X_negative = list(filter( reject_positive, X_negative ))[:n_negative]
+    y_negative = np.zeros(n_negative)
+
+    X = np.concatenate(( X_positive, X_negative ))
+    y = np.concatenate((y_positive, y_negative ))
+
     clf = MLPClassifier(solver='lbfgs', alpha=1e-5, activation='relu',
-                        hidden_layer_sizes=(5,), random_state=1)
+                        hidden_layer_sizes=(100,), random_state=1)
     clf.fit( X, y )
     test_samples = sampleRandomPointsFromRect(500, x_lims, y_lims)
     preds = clf.predict( test_samples )
