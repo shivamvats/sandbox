ddf8b84dc4233e9cf9d2d50327ca38aedf541b83
Shivam Vats <shivamvats.iitkgp@gmail.com>
2020-05-14 14:32:53 -0400

    Can declare global variable from inside a function

--- python/global.py
+++ python/global.py
@@ -6,5 +6,11 @@ def fn():
     as long as it is only reading it and not writing on it."""
     print(x)
 
+def bar():
+    global y
+    y = 10
+
 if __name__ == "__main__":
     fn()
+    bar()
+    print(y)
